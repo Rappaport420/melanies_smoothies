4530cf8b1068d0aad4a07973cba0e8d207014d27
Ivan Lejko <62396752+Rappaport420@users.noreply.github.com>
2026-03-18 21:29:03 +0100

    Update streamlit_app.py
    Hot fix get_active_session

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title("Custome Smothies or whatever...")
 
-session = get_active_session()
+cnx = st. connection("snowflake")
+session = cnx. session)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
