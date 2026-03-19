9d5c1932414ea393758723010e9fbe6418b35b61
Ivan Lejko <62396752+Rappaport420@users.noreply.github.com>
2026-03-19 20:54:41 +0100

    Update streamlit_app.py
    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.title("Custome Smothies or whatever...")
 
 cnx = st. connection("snowflake")
 session = cnx. session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 smoothie_name = st.text_input("Name on Smoothie")
